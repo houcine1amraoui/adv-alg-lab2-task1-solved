1d42950facbf09cc4dd5ea1d8922a251bad382ce
houcine1amraoui <houcine1amraoui@gmail.com>
2025-11-27 11:10:44 +0100

    updated

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from utils import sum_array
 from timeit import timeit
 from random import randint
 import matplotlib.pyplot as plt
-
 sizes = [100, 1000, 10000]
 times1 = []
 times2 = []
-
 for size in sizes:
-    # complete the loop
-    pass
+    arr = [randint(0, 10) for i in range(size)]
+    time1 = timeit(lambda: sum_array(arr), number=10)/10
+    time2 = timeit(lambda: sum(arr), number=10)/10
+    times1.append(time1)
+    times2.append(time2)
 
 plt.plot(sizes, times1, label="my sum")
 plt.plot(sizes, times2, label="python sum")
--- utils.py
+++ utils.py
@@ -3,4 +3,6 @@ def sum_array(arr):
     return total sum of array elements
     """
     total = 0
-    # complete code below
+    for number in arr:
+        total += number
+    return total
